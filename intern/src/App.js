c67a45024bc771ccbfa77f8333a51ca07381cd31
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-05-17 12:23:31 +0530

    added handbag section

---
+++ intern/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Handbags from './components/handbag/handbags';
+
+function App() {
+  return (
+    <div className="App">
+      <Handbags/>
+    </div>
+  );
+}
+
+export default App;
