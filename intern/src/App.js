8a931bca962d0917c7d4096b5ee00a94508a41d1
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-05-19 12:54:02 +0530

    added dashboard section

--- intern/src/App.js
+++ intern/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Handbags from './components/handbag/handbags';
 import Shop from './components/shop_by_catagories/shop';
 import Carousel from './components/carousel/carousel';
+import Dashboard from './components/dashboard/dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Handbags/>
+      {/* <Handbags/>
       <Shop/>
-      <Carousel/>
+      <Carousel/> */}
+      <Dashboard/>
     </div>
   );
 }
